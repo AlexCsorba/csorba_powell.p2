cddb9d3531afd3fda5b058572bfc84ca112c714d
Alex <Alex@DESKTOP-QTNG408>
2023-03-28 14:13:14 -0400

    working on ms2

--- src/mastermind/CodeMaker.java
+++ src/mastermind/CodeMaker.java
@@ -1,14 +1,6 @@
 package mastermind;
 
-/**
- * Provides an interface for code makers.
- * @author Joe Meehean
- *
- */
 public interface CodeMaker {
-    /**
-     * Generates a code.
-     * @return the code generated
-     */
+
     public Code generateCode();
 }
--- src/mastermind/EliminationBreaker.java
+++ src/mastermind/EliminationBreaker.java
@@ -1,23 +1,69 @@
 package mastermind;
 import java.util.List;
-public class EliminationBreaker implements CodeBreaker{
+//public class EliminationBreaker implements CodeBreaker{
+//
+//    private int length;
+//    private int range;
+//
+//    public EliminationBreaker(int codeLength, int codeRange){
+//        length = codeLength;
+//        range = codeRange;
+//    }
+//    public Code nextGuess() {
+//        return null;
+//    }
+//
+//    public void guessResults(Code guess, Code.Results results) {
+//
+//    }
+//
+//    public int possibleCodeCount() {
+//        return 0;
+//    }
+//}
 
-    private int length;
-    private int range;
+public class EliminationBreaker implements CodeBreaker {
 
-    public EliminationBreaker(int codeLength, int codeRange){
-        length = codeLength;
-        range = codeRange;
+    private final int codeLength; // the length of the secret code
+    private final int codeRange; // the range of possible values for each digit
+    private int remainingCodes; // the number of remaining possible codes
+    private boolean[] eliminatedCodes; // an array to keep track of which codes have been eliminated
+
+
+    public EliminationBreaker(int codeLength, int codeRange) {
+        this.codeLength = codeLength;
+        this.codeRange = codeRange;
+        this.remainingCodes = (int) Math.pow(codeRange, codeLength);
+        this.eliminatedCodes = new boolean[remainingCodes];
     }
+
+
     public Code nextGuess() {
-        return null;
+        int guess = 0;
+        while (eliminatedCodes[guess]) {
+            guess++;
+        }
+        eliminatedCodes[guess] = true;
+        remainingCodes--;
+        return new Code(guess, codeLength, codeRange);
     }
+
 
     public void guessResults(Code guess, Code.Results results) {
-
+        for (int i = 0; i < eliminatedCodes.length; i++) {
+            if (!eliminatedCodes[i]) {
+                Code c = new Code(i, codeLength, codeRange);
+                Code.Results r = guess.checkGuess(c);
+                if (!r.equals(results)) {
+                    eliminatedCodes[i] = true;
+                    remainingCodes--;
+                }
+            }
+        }
     }
 
+
     public int possibleCodeCount() {
-        return 0;
+        return remainingCodes;
     }
 }
